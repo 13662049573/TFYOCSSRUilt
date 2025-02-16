a89f8b510f2df9d632e3f9b59339ec8b38e10b4f
田风有 <420144542@qq.com>
2025-02-16 21:26:29 +0800

    asf

---
+++ PacketOC/PacketTunnelProvider.h
@@ -0,0 +1,12 @@
+//
+//  PacketTunnelProvider.h
+//  PacketOC
+//
+//  Created by 田风有 on 2025/2/13.
+//
+
+#import <NetworkExtension/NetworkExtension.h>
+
+@interface PacketTunnelProvider : NEPacketTunnelProvider
+
+@end
